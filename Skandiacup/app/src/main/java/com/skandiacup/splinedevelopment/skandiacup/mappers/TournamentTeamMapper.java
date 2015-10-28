586e55ae11da0e4e6527e675433343e674ca5ced
eiriksandberg <eirik.sandberg@live.no>
2015-10-28 16:32:42 +0100

    Added TournamentTeam Mapper, domain and datamanager

---
+++ Skandiacup/app/src/main/java/com/skandiacup/splinedevelopment/skandiacup/mappers/TournamentTeamMapper.java
@@ -0,0 +1,29 @@
+package com.skandiacup.splinedevelopment.skandiacup.mappers;
+import com.skandiacup.splinedevelopment.skandiacup.domain.TournamentTeam;
+import org.ksoap2.serialization.SoapObject;
+import java.util.ArrayList;
+
+/**
+ * Created by eiriksandberg on 28.10.2015.
+ */
+public class TournamentTeamMapper {
+    public static ArrayList<TournamentTeam> mapTournamentTeams(SoapObject soapObject){
+        ArrayList<TournamentTeam> teams = new ArrayList<>();
+
+        for(int i=0; i<soapObject.getPropertyCount(); i++) {
+            SoapObject obj2 = (SoapObject) soapObject.getProperty(i);
+            TournamentTeam tournamentTeam = new TournamentTeam();
+            tournamentTeam.setId(obj2.getPrimitiveProperty("id").toString());
+            tournamentTeam.setName(obj2.getPrimitiveProperty("name").toString());
+            tournamentTeam.setClubId(obj2.getPrimitiveProperty("clubid").toString());
+            tournamentTeam.setMatchGroupId(obj2.getPrimitiveProperty("MatchGroupId").toString());
+            tournamentTeam.setMatchClassId(obj2.getPrimitiveProperty("MatchClassId").toString());
+            tournamentTeam.setTournamentTeamRegistrationId(obj2.getPrimitiveProperty("TournamentTeamRegistrationId").toString());
+            tournamentTeam.setUpdateTimestamp(obj2.getPrimitiveProperty("UpdateTimestamp").toString());
+            tournamentTeam.setShirtColor(obj2.getPrimitiveProperty("ShirtColor").toString());
+            tournamentTeam.setCountryCode(obj2.getPrimitiveProperty("countryCode").toString());
+            teams.add(tournamentTeam);
+        }
+        return teams;
+    }
+}
--- Skandiacup/app/src/main/java/com/skandiacup/splinedevelopment/skandiacup/repository/DataManager.java
+++ Skandiacup/app/src/main/java/com/skandiacup/splinedevelopment/skandiacup/repository/DataManager.java
@@ -3,7 +3,9 @@ package com.skandiacup.splinedevelopment.skandiacup.repository;
 import com.skandiacup.splinedevelopment.skandiacup.SoapCallback;
 import com.skandiacup.splinedevelopment.skandiacup.SoapRequestCallback;
 import com.skandiacup.splinedevelopment.skandiacup.domain.Arena;
+import com.skandiacup.splinedevelopment.skandiacup.domain.TournamentTeam;
 import com.skandiacup.splinedevelopment.skandiacup.mappers.ArenasMapper;
+import com.skandiacup.splinedevelopment.skandiacup.mappers.TournamentTeamMapper;
 
 import org.ksoap2.serialization.SoapObject;
 
@@ -47,4 +49,42 @@ public class DataManager {
 
         req.execute(params.toArray(new String[params.size()]));
     }
+
+
+     public void getTournamentTeams(String clubId, String limit, String offset, String timestampSince, final SoapCallback<ArrayList<TournamentTeam>> callback){
+         List <String> params = new ArrayList<>();
+         params.add("getTeams");
+         if (clubId != null){
+             params.add("clubId");
+             params.add(clubId);
+         }
+         if(limit != null){
+             params.add("limit");
+             params.add(limit);
+         }
+         if(offset != null){
+             params.add("offset");
+             params.add(offset);
+         }
+         if(timestampSince != null){
+             params.add("timestamp_since");
+             params.add(timestampSince);
+         }
+
+         SoapRequest req = new SoapRequest(new SoapRequestCallback() {
+             @Override
+             public void successCallback(Object data) {
+                 ArrayList<TournamentTeam> teams = TournamentTeamMapper.mapTournamentTeams((SoapObject) data);
+                 callback.successCallback(teams);
+             }
+
+             @Override
+             public void errorCallback() {
+                 callback.errorCallback();
+             }
+         });
+
+         req.execute(params.toArray(new String[params.size()]));
+     }
 }
+
