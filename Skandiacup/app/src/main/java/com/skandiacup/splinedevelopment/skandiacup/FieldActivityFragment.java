aad974201c4fc6580853257feadc547ba473ba40
borgarlie <borgar.lie@gmail.com>
2015-10-28 16:38:17 +0100

    Implemented RSS feed backend

--- Skandiacup/app/src/main/java/com/skandiacup/splinedevelopment/skandiacup/FieldActivityFragment.java
+++ Skandiacup/app/src/main/java/com/skandiacup/splinedevelopment/skandiacup/FieldActivityFragment.java
@@ -7,6 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.skandiacup.splinedevelopment.skandiacup.domain.RSSObject;
+import com.skandiacup.splinedevelopment.skandiacup.repository.DataManager;
+
+import java.util.ArrayList;
+
 /**
  * Created by Jorgen on 27/10/15.
  */
@@ -17,6 +22,19 @@ public class FieldActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        DataManager.getInstance().getRSSFeed(new SoapCallback<ArrayList<RSSObject>>() {
+            @Override
+            public void successCallback(ArrayList<RSSObject> data) {
+                System.out.println(data);
+            }
+
+            @Override
+            public void errorCallback() {
+                System.out.println("ERROR in getting RSS feed");
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_field, container, false);
     }
 }
--- Skandiacup/app/src/main/java/com/skandiacup/splinedevelopment/skandiacup/repository/DataManager.java
+++ Skandiacup/app/src/main/java/com/skandiacup/splinedevelopment/skandiacup/repository/DataManager.java
@@ -1,12 +1,20 @@
 package com.skandiacup.splinedevelopment.skandiacup.repository;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.skandiacup.splinedevelopment.skandiacup.SoapCallback;
 import com.skandiacup.splinedevelopment.skandiacup.SoapRequestCallback;
 import com.skandiacup.splinedevelopment.skandiacup.domain.Arena;
+import com.skandiacup.splinedevelopment.skandiacup.domain.RSSObject;
 import com.skandiacup.splinedevelopment.skandiacup.mappers.ArenasMapper;
+import com.skandiacup.splinedevelopment.skandiacup.mappers.RSSMapper;
 
 import org.ksoap2.serialization.SoapObject;
+import org.xmlpull.v1.XmlPullParserException;
 
+import cz.msebera.android.httpclient.Header;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,4 +55,30 @@ public class DataManager {
 
         req.execute(params.toArray(new String[params.size()]));
     }
+
+    public void getRSSFeed(final SoapCallback<ArrayList<RSSObject>> callback) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "http://skandiacup.no/feed/";
+        client.post(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                // called when response HTTP status is "200 OK"
+                ArrayList<RSSObject> arr;
+                try {
+                    arr = RSSMapper.mapRSS(response);
+                    callback.successCallback(arr);
+                } catch (XmlPullParserException | IOException e) {
+                    e.printStackTrace();
+                    callback.errorCallback();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                callback.errorCallback();
+            }
+        });
+    }
+
 }
