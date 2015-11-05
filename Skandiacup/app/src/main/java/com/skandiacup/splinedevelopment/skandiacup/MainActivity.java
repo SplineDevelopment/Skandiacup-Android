feb7eaa0fc249733052f8d2f75a4c8c739417b36
borgarlie <borgar.lie@gmail.com>
2015-11-05 13:22:59 +0100

    Proper header is set in main view, added a null check for getContext() in news (app will crash if getContext is called after view is gone (i.e. pressed back button before async calls are finished).

--- Skandiacup/app/src/main/java/com/skandiacup/splinedevelopment/skandiacup/MainActivity.java
+++ Skandiacup/app/src/main/java/com/skandiacup/splinedevelopment/skandiacup/MainActivity.java
@@ -43,41 +43,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         //Removing our actionbar (OK to do it here?
         ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-*/
+        if (actionBar != null) {
+            //actionBar.hide();
+        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
