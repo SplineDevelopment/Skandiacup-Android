c4e2907322b2cf25ed5cee996b2f20346ba29a07
Jørgen Wilhelmsen <jorgenwilhelmsen1@gmail.com>
2015-12-15 11:24:49 +0100

    Displaying match class and match group name where it should be displayed.

--- Skandiacup/app/src/main/java/com/skandiacup/splinedevelopment/skandiacup/MatchClassActivity.java
+++ Skandiacup/app/src/main/java/com/skandiacup/splinedevelopment/skandiacup/MatchClassActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -23,6 +25,10 @@ public class MatchClassActivity extends AppCompatActivity {
         setContentView(R.layout.activity_match_class);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar a = getSupportActionBar();
+        if (a != null) {
+            a.setDisplayHomeAsUpEnabled(true);
+        }
         ListView lv = (ListView) findViewById(R.id.matchGroupsList);
         final MatchClass selectedClass = (MatchClass) getIntent().getSerializableExtra("MatchClass");
         setTitle(selectedClass.getName());
@@ -38,8 +44,18 @@ public class MatchClassActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(getApplicationContext(), TeamActivity.class);
                 intent.putExtra("matchGroup", selectedClass.getMatchGroups().get(position));
+                intent.putExtra("matchClassName", selectedClass.getName() + " - " + selectedClass.getMatchGroups().get(position).getName());
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
